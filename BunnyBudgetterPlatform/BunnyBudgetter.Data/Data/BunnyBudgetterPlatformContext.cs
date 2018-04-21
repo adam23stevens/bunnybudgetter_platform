9dfa1f193dc412203ccc049ce07a3b17d248dd43
Adam <adam23stevens@gmail.com>
2018-04-21 20:43:37 +0100

    added controller

---
+++ BunnyBudgetterPlatform/BunnyBudgetter.Data/Data/BunnyBudgetterPlatformContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BunnyBudgetter.Data;
+
+namespace BunnyBudgetterPlatform.Data.Models
+{
+    public class BunnyBudgetterPlatformContext : DbContext
+    {
+        public BunnyBudgetterPlatformContext (DbContextOptions<BunnyBudgetterPlatformContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BunnyBudgetter.Data.User> User { get; set; }
+    }
+}
--- BunnyBudgetterPlatform/BunnyBudgetterPlatform/Startup.cs
+++ BunnyBudgetterPlatform/BunnyBudgetterPlatform/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using BunnyBudgetterPlatform.Models;
 
 namespace BunnyBudgetterPlatform
 {
@@ -24,6 +26,11 @@ namespace BunnyBudgetterPlatform
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<BunnyBudgetterPlatformContext>(options =>
+                    options.UseSqlite(
+                        Configuration.GetConnectionString("BunnyBudgetterPlatformContext")
+                        , b => b.MigrationsAssembly("BunnyBudgetter.Data")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
